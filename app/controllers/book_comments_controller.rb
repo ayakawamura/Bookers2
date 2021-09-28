121a0fd7df00a39f7d9cd83b5095bbbd949e0fa4
ayakawamura <ayakawamura.b@gmail.com>
2021-09-28 23:26:25 +0000

    [update]コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,23 +6,24 @@ class BookCommentsController < ApplicationController
     book_comment=current_user.book_comments.new(book_comment_params)
     book_comment.book_id=@book.id
     book_comment.save
-    redirect_back(fallback_location: root_path) 
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     BookComment.find_by(id:params[:id]).destroy
-    redirect_back(fallback_location: root_path) 
+    redirect_back(fallback_location: root_path)
   end
-  
+
   private
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
+  # destroyの
   def ensure_correct_user
     @book=BookComment.find_by(id:params[:id])
     unless @book.user == current_user
-      redirect_back(fallback_location: root_path) 
+      redirect_back(fallback_location: root_path)
     end
   end
 
